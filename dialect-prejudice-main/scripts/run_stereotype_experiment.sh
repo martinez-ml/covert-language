6b61f4bfe93a2e77ae174925936466a81c0b311d
Joseph Martinez <f007hvb@a01.hpcc.dartmouth.edu>
2025-05-01 12:45:41 -0400

    llama 4 added with arabic

--- dialect-prejudice-main/scripts/run_stereotype_experiment.sh
+++ dialect-prejudice-main/scripts/run_stereotype_experiment.sh
@@ -3,10 +3,11 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 #arb_Arab fra_Latn heb_Hebr jpn_Jpan rus_Cyrl spa_Latn zho_Hans
+#arabic chinese french hebrew japanese russian spanish
 #google/gemma-2b deepseek-ai/deepseek-llm-7b-base meta-llama/Meta-Llama-3-8B
-for model in google/gemma-2b deepseek-ai/deepseek-llm-7b-base meta-llama/Meta-Llama-3-8B
+for model in meta-llama/Llama-Guard-4-12B
     do
-    for variable in arabic chinese french hebrew japanese russian spanish
+    for variable in arb_Arab arabic
     do
         python -u ../probing/mgp.py \
         --model $model \
