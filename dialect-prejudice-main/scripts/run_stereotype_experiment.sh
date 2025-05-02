db35920b294ea08d351d6edaf7779104a577ae09
Joseph Martinez <f007hvb@a02.hpcc.dartmouth.edu>
2025-05-02 13:14:30 -0400

    add microsoft/Phi-4-mini-reasoning

--- dialect-prejudice-main/scripts/run_stereotype_experiment.sh
+++ dialect-prejudice-main/scripts/run_stereotype_experiment.sh
@@ -5,7 +5,7 @@ cd "$parent_path"
 #arb_Arab fra_Latn heb_Hebr jpn_Jpan rus_Cyrl spa_Latn zho_Hans
 #arabic chinese french hebrew japanese russian spanish
 #google/gemma-2b deepseek-ai/deepseek-llm-7b-base meta-llama/Meta-Llama-3-8B
-for model in meta-llama/Llama-Guard-4-12B
+for model in microsoft/Phi-4-mini-reasoning
     do
     for variable in arb_Arab arabic
     do
