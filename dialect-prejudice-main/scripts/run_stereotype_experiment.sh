d687f9ee2ff6fd1b3a13e16fac1570a5f59c64c6
Joseph Martinez <f007hvb@a01.hpcc.dartmouth.edu>
2025-05-02 21:58:10 -0400

    add full phi model

--- dialect-prejudice-main/scripts/run_stereotype_experiment.sh
+++ dialect-prejudice-main/scripts/run_stereotype_experiment.sh
@@ -4,8 +4,8 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 #arb_Arab fra_Latn heb_Hebr jpn_Jpan rus_Cyrl spa_Latn zho_Hans
 #arabic chinese french hebrew japanese russian spanish
-#google/gemma-2b deepseek-ai/deepseek-llm-7b-base meta-llama/Meta-Llama-3-8B
-for model in microsoft/Phi-4-mini-reasoning
+#google/gemma-2b deepseek-ai/deepseek-llm-7b-base meta-llama/Meta-Llama-3-8B microsoft/Phi-4-mini-reasoning
+for model in microsoft/Phi-4-reasoning
     do
     for variable in arb_Arab arabic
     do
