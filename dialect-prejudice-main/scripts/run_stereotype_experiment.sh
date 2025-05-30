4b8666635ba5349819e44f7a8c72b97c2f5c6fc4
Joseph Martinez <f007hvb@slurm-fe01-prd.dartmouth.edu>
2025-05-30 10:35:55 -0400

    add partial results for occupation with new dataset

--- dialect-prejudice-main/scripts/run_stereotype_experiment.sh
+++ dialect-prejudice-main/scripts/run_stereotype_experiment.sh
@@ -4,10 +4,11 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 #arb_Arab fra_Latn heb_Hebr jpn_Jpan spa_Latn zho_Hans
 #arabic chinese french hebrew japanese spanish
+#en-ar_combined en-es_combined en-fr_combined en-he_combined en-ja_combined en-zh_combined 
 #google/gemma-2b deepseek-ai/deepseek-llm-7b-base meta-llama/Meta-Llama-3-8B microsoft/Phi-4-reasoning
-for model in deepseek-ai/deepseek-llm-7b-base meta-llama/Meta-Llama-3-8B
+for model in google/gemma-2b microsoft/Phi-4-reasoning
     do
-    for variable in jpn_Jpan spa_Latn zho_Hans
+    for variable in en-ar_combined en-es_combined en-fr_combined en-he_combined en-ja_combined en-zh_combined 
     do
         python -u ../probing/mgp.py \
         --model $model \
